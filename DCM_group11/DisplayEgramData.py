1050b82f22c99921b2d717cbd4c9bec3f19356a1
dn0830 <nguyed81@mcmaster.ca>
2022-11-25 15:26:44 -0500

    Create DisplayEgramData.py
    Created framework for displaying Egram Data (unfinished)

---
+++ DCM_group11/DisplayEgramData.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as graph
+import random #remove after we have real data (was only used for testing)
+
+x = [] #x-axis
+y = [] #y-axis
+t = 0.0 #time
+
+for i in range(69): #range will change depending on number of inputs
+    x.append(t)
+    t += 0.001 #setting up x axis (change 1ms to whatever correct sampling interval)
+    data = random.randint(0,30) #replace this with sampling data
+    y.append(data) 
+    print(data)#just to test
+
+graph.plot(x, y)
+graph.title('Egram Data from Pacemaker')
+graph.xlabel('Time(s)') #change if its different i wasn't sure
+graph.ylabel('Voltage(mV)')  #change if its different i wasn't sure
+graph.savefig('EgramData.png')
+
